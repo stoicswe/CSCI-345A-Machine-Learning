c12dcd397c053208fc168ba123f8468002ee5fb6
Nathan Bunch <31823043+taranoshi@users.noreply.github.com>
2018-11-01 13:11:08 -0400

    Added the acutal double q learning

--- Group HW2/Double_Q_Learning.py
+++ Group HW2/Double_Q_Learning.py
@@ -7,8 +7,6 @@ env = gym.make("FrozenLake-v0")
 
 Q_1 = np.zeros([env.observation_space.n, env.action_space.n])
 Q_2 = np.zeros([env.observation_space.n, env.action_space.n])
-Q_3 = np.zeros([env.observation_space.n, env.action_space.n])
-QS = [Q_1, Q_2, Q_3]
 num_episodes = 20000
 rList = []
 gamma = 0.99
@@ -19,17 +17,12 @@ for i in range(num_episodes):
     rAll = 0
     done = False
     while not done:
-        action = np.argmax(Q_1[state, :] + Q_2[state, :] + Q_3[state, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+        action = np.argmax(Q_1[state, :] + Q_2[state, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
         new_state, reward, done, _ = env.step(action)
-        x = np.random.rand()
-        if x > 0.6:
-            # Q_1[state, action] = Q_1[state, action] + alpha * (reward + gamma * Q_2[new_state, np.argmax(Q_1[new_state, :])] - Q_1[state, action])
-            Q_1[state, action] = Q_1[state, action] + alpha * (reward + gamma * ((QS[1] + QS[2]) / (len(QS) - 1))[new_state, np.argmax(Q_1[new_state, :])] - Q_1[state, action])
-        elif x > 0.3:
-            Q_2[state, action] = Q_2[state, action] + alpha * (reward + gamma * ((QS[0] + QS[2]) / (len(QS) - 1))[new_state, np.argmax(Q_2[new_state, :])] - Q_2[state, action])
-            # Q_2[state, action] = Q_2[state, action] + alpha * (reward + gamma * Q_1[new_state, np.argmax(Q_2[new_state, :])] - Q_2[state, action])
+        if np.random.rand() > 0.5:
+            Q_1[state, action] = Q_1[state, action] + alpha * (reward + gamma * Q_2[new_state, np.argmax(Q_1[new_state, :])] - Q_1[state, action])
         else:
-            Q_3[state, action] = Q_3[state, action] + alpha * (reward + gamma * ((QS[0] + QS[1]) / (len(QS) - 1))[new_state, np.argmax(Q_3[new_state, :])] - Q_3[state, action])
+            Q_2[state, action] = Q_2[state, action] + alpha * (reward + gamma * Q_1[new_state, np.argmax(Q_2[new_state, :])] - Q_2[state, action])
         rAll += reward
         state = new_state
     rList.append(rAll)
